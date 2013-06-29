cfccd786ec4b6b956ab0dfb0b111718ffbfe78ec
sos0 <so.c.choi@gmail.com>
2013-06-28 23:19:20 -0700

    implemented swipe view

--- src/com/example/liveabetes/MainActivity.java
+++ src/com/example/liveabetes/MainActivity.java
@@ -1,22 +1,52 @@
 package com.example.liveabetes;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-//import android.widget.EditText;
-import android.view.Window;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
+     * three primary sections of the app. We use a {@link android.support.v4.app.FragmentPagerAdapter}
+     * derivative, which will keep every loaded fragment in memory. If this becomes too memory
+     * intensive, it may be best to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    AppSectionsPagerAdapter mAppSectionsPagerAdapter;
 
+    /**
+     * The {@link ViewPager} that will display the three primary sections of the app, one at a
+     * time.
+     */
+    ViewPager mViewPager;
+    
+    // debug TAGs
+    private static final String TAG = ":( :( :( :( :((((( fuq";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		/* remove top title bar cuz it's ugly */
-//		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
+          
+        // Set up the ViewPager with the sections adapter.  
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
 	}
 
 	@Override
@@ -25,6 +55,81 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	/** 
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to 
+     * one of the sections/tabs/pages. 
+     */  
+    public class AppSectionsPagerAdapter extends FragmentPagerAdapter {  
+  
+        public AppSectionsPagerAdapter(FragmentManager fm) {  
+            super(fm);
+        }  
+  
+        @Override  
+        public Fragment getItem(int position) {  
+            // getItem is called to instantiate the fragment for the given page.  
+            // Return a DummySectionFragment (defined as a static inner class  
+            // below) with the page number as its lone argument.  
+        	Fragment fragment = new Fragment();
+        	Log.i(TAG, "received an exception");
+            switch (position) {  
+            case 0:  
+                return fragment = new LeftFragment();  
+            case 1:  
+                return fragment = new CenterFragment();  
+            case 2:  
+                return fragment = new RightFragment();  
+            default:  
+                break;  
+            }
+			return fragment;  
+        }
+  
+        @Override  
+        public int getCount() {  
+            // Show 3 total pages.  
+            return 3;
+        }  
+  
+        @Override  
+        public CharSequence getPageTitle(int position) {  
+            /*Locale l = Locale.getDefault();  
+            switch (position) {  
+            case 0:  
+                return getString(R.string.title_section1).toUpperCase(l);  
+            case 1:  
+                return getString(R.string.title_section2).toUpperCase(l);  
+            case 2:  
+                return getString(R.string.title_section3).toUpperCase(l);  
+            }  
+            return null;  */
+        	return "Section " + (position + 1);
+        }  
+    }  
+  
+    /** 
+     * A dummy fragment representing a section of the app, but that simply 
+     * displays dummy text. 
+     */  
+    public static class DummySectionFragment extends Fragment {  
+        /** 
+         * The fragment argument representing the section number for this 
+         * fragment. 
+         */  
+        public static final String ARG_SECTION_NUMBER = "section_number";  
+  
+        public DummySectionFragment() {
+        }  
+  
+        @Override  
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {  
+            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }  
+    }
 	
 	/** Called when the user clicks the Blood Glucose button */
 	public void bloodGlucose(View view) {
@@ -60,5 +165,23 @@ public class MainActivity extends Activity {
 	public void settings(View view) {
 		Intent intent = new Intent(this, SettingsActivity.class);
 	    startActivity(intent);
+	}
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ src/com/example/liveabetes/RightFragment.java
@@ -0,0 +1,18 @@
+package com.example.liveabetes;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RightFragment extends Fragment{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		ViewGroup rootView = (ViewGroup) inflater.inflate(
+				R.layout.swipeview_fragment_right, container, false);
+
+		return rootView;
+	}
+}
