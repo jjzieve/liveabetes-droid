fc3de437886a1f1900cce84c1c778bc4b6d4d5a1
sos0 <so.c.choi@gmail.com>
2013-06-30 21:47:20 -0700

    fixed skip crash

--- src/com/example/liveabetes/ProfileSetupActivity.java
+++ src/com/example/liveabetes/ProfileSetupActivity.java
@@ -2,11 +2,8 @@ package com.example.liveabetes;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class ProfileSetupActivity extends Activity {
 	@Override
